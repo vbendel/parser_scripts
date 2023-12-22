46c4f146b754b61c454593602862bdf93b540be1
Vratislav Bendel <vbendel@redhat.com>
2023-12-22 14:20:25 +0100

    page_owner_parse: Parse PID list and CMD count
    Also run the script through black formatter
    
    Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

--- page_owner_parse.py
+++ page_owner_parse.py
@@ -3,11 +3,11 @@ import sys
 
 
 def get_function_name(bt_line):
-    return re.search(r'(\w+)+', bt_line).group(1)
+    return re.search(r"(\w+)+", bt_line).group(1)
 
 
 def get_module_name(bt_line):
-    match = re.search(r'\[(\w+)\]', bt_line)
+    match = re.search(r"\[(\w+)\]", bt_line)
     if match:
         return match.group(1)
     else:
@@ -15,9 +15,9 @@ def get_module_name(bt_line):
 
 
 def count_allocation_backtrace_pairs(filename):
-    allocation_backtrace_counts = {}
+    allocation_data = {}
 
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         lines = file.readlines()
         i = 0
         while i < len(lines):
@@ -26,7 +26,9 @@ def count_allocation_backtrace_pairs(filename):
             # Check if it is the start of a record
             if line.startswith("Page allocated via order"):
                 allocation_info = line
-                order = re.search(r'order (\d+)', allocation_info).group(1)
+                pid = re.search(r"pid (\d+)", allocation_info).group(1)
+                cmd_name = re.search(r"tgid \d+ \((.*)\)", allocation_info).group(1)
+                order = re.search(r"order (\d+)", allocation_info).group(1)
                 backtrace = ""
                 j = i + 2
 
@@ -42,22 +44,31 @@ def count_allocation_backtrace_pairs(filename):
                     j += 1
 
                 # Filter for tuples with backtrace containing a module
-                #if module != "":
-                
+                # if module != "":
+
                 # Create the order, and backtrace tuple
                 allocation_tuple = (int(order), backtrace)
+                pid_cmd_tuple = (int(pid), cmd_name)
 
                 # Update the count for the tuple
-                if allocation_tuple in allocation_backtrace_counts:
-                    allocation_backtrace_counts[allocation_tuple] += 1
+                if allocation_tuple in allocation_data:
+                    data = allocation_data[allocation_tuple]
+                    data["count"] += 1
+                    if pid_cmd_tuple in data["pid_cmd_data"]:
+                        data["pid_cmd_data"][pid_cmd_tuple] += 1
+                    else:
+                        data["pid_cmd_data"][pid_cmd_tuple] = 1
                 else:
-                    allocation_backtrace_counts[allocation_tuple] = 1
+                    allocation_data[allocation_tuple] = {
+                        "count": 1,
+                        "pid_cmd_data": {pid_cmd_tuple: 1},
+                    }
 
                 i = j  # Skip the processed lines
             else:
                 i += 1
 
-    return allocation_backtrace_counts
+    return allocation_data
 
 
 # Check if the filename is provided as a command-line argument
@@ -66,10 +77,23 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 filename = sys.argv[1]
-counts = count_allocation_backtrace_pairs(filename)
+alloc_data = count_allocation_backtrace_pairs(filename)
 
 # Print the tuple counts
-for allocation_tuple, count in counts.items():
+for allocation_tuple, data in alloc_data.items():
     order, backtrace = allocation_tuple
-    result = 2 ** order * count * 4
-    print(f'Result: {result} KB : Count: {count} | Order: {order} | {backtrace}')
+    count = data["count"]
+    pid_cmd_data = data["pid_cmd_data"]
+    result = 2**order * count * 4
+    pid_string = ""
+    cmd_cnt = {}
+    for pid_cmd, pcount in pid_cmd_data.items():
+        pid, cmd = pid_cmd
+        pid_string += f"{pid}[{cmd}]:{pcount} "
+        if cmd in cmd_cnt:
+            cmd_cnt[cmd] += 1
+        else:
+            cmd_cnt[cmd] = 1
+    print(
+        f"Result: {result} KB : Count: {count} | Order: {order} | {backtrace} | CMD count: {cmd_cnt} | PID list: {pid_string}"
+    )
