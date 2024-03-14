b3702fe373816ac66c288af8e1babdc72d1e942b
Vratislav Bendel <vbendel@redhat.com>
2024-03-14 09:01:04 +0100

    page_owner: Add a no-PID version
    Older versions of page_owner may not collect PID info.
    
    Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

---
+++ page_owner_parse_noPID.py
@@ -0,0 +1,78 @@
+import re
+import sys
+
+
+def get_module_name(bt_line):
+    match = re.search(r"\[(\w+)\]", bt_line)
+    if match:
+        return match.group(1)
+    else:
+        return ""
+
+
+def count_allocation_backtrace_pairs(filename):
+    allocation_data = {}
+
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+
+            # Check if it is the start of a record
+            if line.startswith("Page allocated via order"):
+                allocation_info = line
+                order = re.search(r"order (\d+)", allocation_info).group(1)
+                backtrace = ""
+                j = i + 2
+
+                # Extract the backtrace until an empty line is encountered
+                module = ""
+                while j < len(lines) and lines[j].strip() != "":
+                    backtrace += lines[j].strip().split(' ')[1].split('+')[0]
+                    module = get_module_name(lines[j].strip())
+                    if module != "":
+                        backtrace += f"--[{module}] "
+                    else:
+                        backtrace += " "
+                    j += 1
+
+                # Filter for tuples with backtrace containing a module
+                # if module != "":
+
+                # Create the order, and backtrace tuple
+                allocation_tuple = (int(order), backtrace)
+
+                # Update the count for the tuple
+                if allocation_tuple in allocation_data:
+                    data = allocation_data[allocation_tuple]
+                    data["count"] += 1
+                else:
+                    allocation_data[allocation_tuple] = {
+                        "count": 1,
+                    }
+
+                i = j  # Skip the processed lines
+            else:
+                i += 1
+
+    return allocation_data
+
+
+# Check if the filename is provided as a command-line argument
+if len(sys.argv) != 2:
+    print("Please provide the input file as a command-line argument.")
+    sys.exit(1)
+
+filename = sys.argv[1]
+alloc_data = count_allocation_backtrace_pairs(filename)
+
+# Print the tuple counts
+for allocation_tuple, data in alloc_data.items():
+    order, backtrace = allocation_tuple
+    count = data["count"]
+    result = 2**order * count * 4
+    cmd_cnt = {}
+    print(
+        f"Result: {result} KB : Count: {count} | Order: {order} | {backtrace}"
+    )
